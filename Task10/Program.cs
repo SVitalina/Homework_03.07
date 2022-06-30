644650e595f02a93b3ee13dbe9ac093251b6bf9a
Oleksii <zankexd@icloud.com>
2022-06-30 17:25:02 +0200

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int a = new Random().Next(100, 999);
+int centre = 0; 
+int centre1 = 0; 
+
+centre = a%100;
+centre1 = centre/10;
+
+
+Console.WriteLine(a);
+Console.WriteLine(centre1);
